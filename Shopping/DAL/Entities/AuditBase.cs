4785f57eb5d41f780119ccfd34473d167f50f68b
DrZEROpss <juandiegopasos12@gmail.com>
2025-05-19 16:04:52 -0500

    setup

---
+++ Shopping/DAL/Entities/AuditBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.DAL.Entities
+{
+    public class AuditBase
+    {
+        [Key] //pk
+        [Required] //obligatorio
+        public virtual Guid Id { get; set;  } //generar clave Pk de todas las tablas
+        public virtual DateTime? CreatedDate { get; set; } //guardar fecha de creacion
+
+        public virtual DateTime? ModifiedDate { get; set; } //guardar fecha de modificacion
+
+    }
+}
