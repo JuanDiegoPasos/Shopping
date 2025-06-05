d481e66604b5edda3de928ab7f27cfe158ccd410
DrZEROpss <juandiegopasos12@gmail.com>
2025-06-05 15:51:42 -0500

    ultima entrega

---
+++ Shopping/DAL/SeederDB.cs
@@ -0,0 +1,81 @@
+﻿using Shopping.DAL.Entities;
+using System.Data;
+
+namespace Shopping.DAL
+{
+    public class SeederDB
+    {
+        private readonly DataBaseContext _context;
+
+        public SeederDB(DataBaseContext context)
+        {
+                _context = context;
+        }
+
+        public async Task SeederAsync() 
+        {
+            //metodo q sustituye update-database, crea la bd inmediatamente cuando se hace la api
+
+            await _context.Database.EnsureCreatedAsync();
+
+            //metodos para prepoblar la bd
+
+            await PopulateCountriesAsync();
+
+            await _context.SaveChangesAsync();
+        }
+
+        #region Private Methods
+
+        private async Task PopulateCountriesAsync() 
+        {
+            if (!_context.Countries.Any())
+            {
+                _context.Countries.Add(new Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Colombia",
+                    States = new List<State>()
+                    {
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Antioquia"
+                        },
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Cundinamarca"
+                        }
+                    }
+
+                });
+
+                _context.Countries.Add(new Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Argentina",
+                    States = new List<State>()
+                    {
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Buenos aires"
+                        },
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Cordoba"
+                        }
+                    }
+
+                });
+
+            }
+        }
+
+        #endregion
+
+
+    }
+}
--- Shopping/Program.cs
+++ Shopping/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics.Internal;
 using Microsoft.EntityFrameworkCore.Storage;
 using Shopping.DAL;
 using Shopping.Domain.Interfaces;
@@ -15,12 +16,27 @@ builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(b
 
 builder.Services.AddScoped<ICountryService, CountryService>();
 builder.Services.AddScoped<IStateService, StateService>();
+builder.Services.AddTransient<SeederDB>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+SeederData();
+
+void SeederData()
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopedFactory.CreateScope())
+    {
+        SeederDB? service = scope.ServiceProvider.GetService<SeederDB>();
+        service.SeederAsync().Wait();
+
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
